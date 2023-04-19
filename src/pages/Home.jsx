6bd5c6b71b91ba654f6cfd9dc1977056f0288bac
BruBobotis <brunoalvesinvest@gmail.com>
2023-04-19 17:18:49 -0300

    Requisito 04 check, criado os componentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
+import Categories from '../components/Categories';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  state = {
+    categoryList: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.fetchCategoryList();
+  }
+
+  fetchCategoryList = async () => {
+    this.setState({ loading: true });
+    const categories = await getCategories();
+    this.setState({ categoryList: categories, loading: false });
+  };
+
   render() {
+    const { categoryList, loading } = this.state;
     return (
       <div>
         <Link
@@ -19,6 +38,20 @@ class Home extends Component {
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="category">
+            {categoryList
+            && categoryList.map((products) => (
+              <Categories
+                key={ products.id }
+                name={ products.name }
+                value={ products.name }
+              />
+            ))}
+          </div>
+        )}
       </div>
     );
   }
