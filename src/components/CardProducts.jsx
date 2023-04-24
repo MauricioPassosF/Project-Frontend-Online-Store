537551914318fb90c76070afe8298c44162f05a8
Lucas Parreiras Romanelli Bueno <lucaspbueno22@gmail.com>
2023-04-24 13:53:43 -0300

    Requisito 10- check Criei a fun cionalidade de aumentar e diminuir a quantidade de produtos e de remove-los

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -3,9 +3,39 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class CardProducts extends Component {
+  state = {
+    quantityState: 1,
+  };
+
+  atualizaQuantidade = () => {
+    const { product: { id } } = this.props;
+    const quantityProduct = JSON.parse(localStorage.getItem('productsList'));
+    const product = quantityProduct.filter((item) => item.id === id)[0];
+    this.setState({
+      quantityState: product.quantity,
+    });
+  };
+
+  addAndSubtractQuantity = (type) => {
+    const { product: { id } } = this.props;
+    const arrayProducts = JSON.parse(localStorage.getItem('productsList'));
+    const produto = arrayProducts.filter((product) => product.id === id)[0];
+    if (type === 'add') {
+      produto.quantity += 1;
+    } else {
+      produto.quantity -= 1;
+    }
+    /* console.log(produto.quantity); */
+    const indexProduct = arrayProducts.findIndex((product) => product.id === id);
+    arrayProducts.splice(indexProduct, 1, produto);
+    localStorage.setItem('productsList', JSON.stringify(arrayProducts));
+    this.atualizaQuantidade();
+  };
+
   render() {
     const {
-      product: { id, title, thumbnail, price, quantity }, addProduct, type } = this.props;
+      product: { id, title, thumbnail, price }, addProduct, type, removeProduct } = this.props;
+    const { quantityState } = this.state;
     return (
       <div key={ id } data-testid="product">
         <Link
@@ -25,11 +55,34 @@ class CardProducts extends Component {
         <img src={ thumbnail } alt={ title } />
         {
           type === 'cart' ? (
-            <p data-testid="shopping-cart-product-quantity">
-              {
-                `Quantidade: ${quantity}`
-              }
-            </p>
+            <div>
+              <p data-testid="shopping-cart-product-quantity">
+                {
+                  `Quantidade: ${quantityState <= 0 ? 1 : quantityState}`
+                }
+              </p>
+              <button
+                type="submit"
+                data-testid="product-increase-quantity"
+                onClick={ () => this.addAndSubtractQuantity('add') }
+              >
+                +
+              </button>
+              <button
+                data-testid="remove-product"
+                type="submit"
+                onClick={ () => removeProduct(id) }
+              >
+                REMOVER
+              </button>
+              <button
+                type="submit"
+                data-testid="product-decrease-quantity"
+                onClick={ () => this.addAndSubtractQuantity() }
+              >
+                -
+              </button>
+            </div>
           ) : ''
         }
         <p>{`R$ ${price}`}</p>
